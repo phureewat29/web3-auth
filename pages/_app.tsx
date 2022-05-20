35c354b22b158b5d2de66bbb8c5a246229449888
Phureewat Aphibansri <a.phureewat@gmail.com>
2022-05-20 16:59:27 +0700

    [feat] with chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,53 @@
-import "../styles/globals.css";
-import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
-import "regenerator-runtime/runtime";
-
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThirdwebProvider } from '@3rdweb/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const supportedChainIds = [1, 4];
+  // Put the ethereum chain ids of the chains you want to support
+  const supportedChainIds = [1, 3, 4, 42]
 
+  // Ethereun Mainnet - 1 (ETH)
+  // Ropsten Test Network - 3 (ETH)
+  // Rinkeby Test Network - 4 (ETH)
+  // Kovan Test Network -  42 (ETH)
+  // Polygon Mumbai Test Network - 80001 (MATIC)
+
+  /**
+   * Include the connectors you want to support
+   * injected - MetaMask
+   * magic - Magic Link
+   * walletconnect - Wallet Connect
+   * walletlink - Coinbase Wallet
+   */
   const connectors = {
     injected: {},
-  };
+    // magic: {
+    //   apiKey: 'pk_...', // Your magic api key
+    //   chainId: 1, // The chain ID you want to allow on magic
+    // },
+    // walletconnect: {},
+    // walletlink: {
+    //   appName: 'web3-auth',
+    //   url: 'http://localhost:3000',
+    //   darkMode: false,
+    // },
+  }
 
+  /**
+   * Make sure that your app is wrapped with these contexts.
+   * If you're using Next JS, you'll have to replace children with the Component setup
+   */
   return (
-    <ThirdwebWeb3Provider
-      supportedChainIds={supportedChainIds}
+    <ThirdwebProvider
       connectors={connectors}
+      supportedChainIds={supportedChainIds}
     >
-      <Component {...pageProps} />
-    </ThirdwebWeb3Provider>
-  );
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,115 @@
 import type { NextPage } from 'next'
-import { useWeb3 } from "@3rdweb/hooks"
+import Head from 'next/head'
+import { useWeb3, useSwitchNetwork } from '@3rdweb/hooks'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from '@chakra-ui/react'
+import { HiChevronDown } from 'react-icons/hi'
+import { IoMdWallet } from 'react-icons/io'
 
-const Home: NextPage = () => {
-  const { connectWallet, address } = useWeb3();
+const NetworkMetadata = (props: {
+  icon: React.ComponentType | string
+  chainName: string
+  symbol: string
+  isTestnet: boolean
+}) => {
+  const { chainName } = props
+  if (!props || !chainName) {
+    return <></>
+  }
+  const { icon, symbol, isTestnet } = props
+
+  const ChainIconElement = icon
+  const iconSize = symbol === 'MATIC' ? 5 : 3
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-100">
+    <Flex gap={2}>
+      <Box w={iconSize} h={iconSize}>
+        <ChainIconElement />
+      </Box>
+      <Text>{chainName}</Text>
+      {isTestnet && <Text color="grey">(testnet)</Text>}
+    </Flex>
+  )
+}
+
+const CustomConnect = () => {
+  const web3 = useWeb3()
+
+  const {
+    address,
+    chainId,
+    connectWallet,
+    disconnectWallet,
+    getNetworkMetadata,
+  } = web3
+  const { switchNetwork } = useSwitchNetwork()
+
+  let networkMetadata = null
+
+  if (chainId) {
+    networkMetadata = getNetworkMetadata(chainId)
+  }
+
+  // If a wallet is connected, show disconnect and switch network options
+  return (
+    <>
       {address ? (
-        <Dashboard address={address} />
+        <Menu>
+          <MenuButton minH={10} as={Button} rightIcon={<HiChevronDown />}>
+            {networkMetadata && <NetworkMetadata {...networkMetadata} />}
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => switchNetwork(3)}>
+              Switch to Ropsten
+            </MenuItem>
+            <MenuItem onClick={() => switchNetwork(4)}>
+              Switch to Rinkeby
+            </MenuItem>
+            <MenuItem onClick={() => switchNetwork(42)}>
+              Switch to Kovan
+            </MenuItem>
+            <MenuItem onClick={disconnectWallet}>Switch Wallet</MenuItem>
+          </MenuList>
+        </Menu>
       ) : (
-        <button className="px-4 py-2 rounded-md cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white"
-          onClick={()=>connectWallet("injected")}>
-          Connect Wallet
-        </button>
+        <>
+          <Button
+            onClick={() => connectWallet('injected')}
+            leftIcon={<IoMdWallet />}
+            colorScheme="purple"
+          >
+            Connect MetaMask
+          </Button>
+        </>
       )}
-    </div>
+    </>
+  )
+}
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Web3 Auth - aquartier</title>
+        <meta name="description" content="Web3 Auth" />
+      </Head>
+      <Container maxW="container.md">
+        <Flex flexDirection="column" w="100%" h="100%">
+          <Flex justifyContent="flex-end" m={5}>
+            <CustomConnect />
+          </Flex>
+        </Flex>
+      </Container>
+    </>
   )
 }
 
