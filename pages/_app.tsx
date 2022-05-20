fc5fb240c8af44f4862f59713751b7c8a5719174
Phureewat Aphibansri <a.phureewat@gmail.com>
2022-05-20 16:23:38 +0700

    [feat] nextjs + tailwind + 3rdweb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import "regenerator-runtime/runtime";
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const supportedChainIds = [1, 4];
+
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
